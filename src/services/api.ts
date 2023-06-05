71217e3c5427199bf481ebe5bb26e689b42f77bf
Matheus Silva <103164463+MSOliver1998@users.noreply.github.com>
2023-06-05 15:28:23 -0300

    fix/modalDelete

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 export const api = axios.create({
-  //baseURL: "https://contacts-back-end.onrender.com",
-  baseURL:'http://localhost:3000',
+  baseURL: "https://contacts-back-end.onrender.com",
+  //baseURL:'http://localhost:3000',
   timeout: 10000,
 })
